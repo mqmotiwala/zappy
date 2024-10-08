18c34fcc9ab46de5509545e97b8c625b6c3b41be
Mufaddal Motiwala <mqmotiwala@gmail.com>
2024-10-07 23:43:27 -0700

    init warn.py

---
+++ warn.py
@@ -0,0 +1,22 @@
+from logger import logger
+from pushover import Pushover
+from state_machine import StateMachine
+
+WARN_MSG = "Reminder: you haven't zapped yet today!"
+
+def warn():
+    """
+        This will send a Warning Pushover notification if not yet zapped today.
+        Cron will trigger this script daily at 9pm.
+    """
+
+    p = Pushover()
+    sm = StateMachine()
+
+    if not sm.zapped_today:
+        p.send_notification(WARN_MSG)
+    else:
+        logger.debug("Zapped already. Skip warning.")
+
+if __name__ == '__main__':
+    warn()
